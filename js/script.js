342edbda764542a1b94ec6e139c812d4fdefa89d
Roman Lytvynenko <zoltiisen@example.com>
2023-09-28 22:29:29 +0300

    Lost code added

--- js/script.js
+++ js/script.js
@@ -1,26 +1,4 @@
 'use strict';
-// Function opens sub menu when mouse is on opener text
-function mouseOverSubMenuOpener(index) {
-	const opener = document.querySelectorAll('.subMenuOpener')[index];
-	opener.querySelector('i').style.transform = 'rotate(180deg)';
-	opener.querySelector('.hidden-line').classList.remove('hidden');
-	opener.querySelector('.hidden-block').classList.remove('hidden');
-	const subMenu = document.querySelectorAll('.subMenu')[index];
-	subMenu.style.transform = 'translateX(-50%) translateY(0)';
-	subMenu.style.opacity = 1;
-	subMenu.style.pointerEvents = 'all';
-}
-// Function closes sub menu when mouse is out of opener text
-function mouseOutSubMenu(index) {
-	const opener = document.querySelectorAll('.subMenuOpener')[index];
-	opener.querySelector('i').style.transform = 'rotate(0deg)';
-	opener.querySelector('.hidden-line').classList.add('hidden');
-	opener.querySelector('.hidden-block').classList.add('hidden');
-	const subMenu = document.querySelectorAll('.subMenu')[index];
-	subMenu.style.transform = 'translateX(-50%) translateY(-100px)';
-	subMenu.style.opacity = 0;
-	subMenu.style.pointerEvents = 'none';
-}
 
 // Function opens navbar menu on small screens
 function openMenu() {
@@ -55,6 +33,65 @@ function closeMenu() {
 	mobileMenu.style.opacity = 0;
 	mobileMenu.style.transform = 'translateY(-100px)';
 	mobileMenu.style.pointerEvents = 'none';
+}
+
+// Function opens sub menu when mouse is on opener text
+function mouseOverSubMenuOpener(index) {
+	const opener = document.querySelectorAll('.subMenuOpener')[index];
+	opener.querySelector('i').style.transform = 'rotate(180deg)';
+	opener.querySelector('.hidden-line').classList.remove('hidden');
+	opener.querySelector('.hidden-block').classList.remove('hidden');
+	const subMenu = document.querySelectorAll('.subMenu')[index];
+	subMenu.style.transform = 'translateX(-50%) translateY(0)';
+	subMenu.style.opacity = 1;
+	subMenu.style.pointerEvents = 'all';
+}
+// Function closes sub menu when mouse is out of opener text
+function mouseOutSubMenu(index) {
+	const opener = document.querySelectorAll('.subMenuOpener')[index];
+	opener.querySelector('i').style.transform = 'rotate(0deg)';
+	opener.querySelector('.hidden-line').classList.add('hidden');
+	opener.querySelector('.hidden-block').classList.add('hidden');
+	const subMenu = document.querySelectorAll('.subMenu')[index];
+	subMenu.style.transform = 'translateX(-50%) translateY(-100px)';
+	subMenu.style.opacity = 0;
+	subMenu.style.pointerEvents = 'none';
+}
+
+// Function opens sub menu on small screen devices
+function openMobileSubMenu(index) {
+	const burger = document.querySelector('.burger-menu');
+	burger.onclick = closeMobileSubMenu;
+	burger.children[0].style.transformOrigin = 'left';
+	burger.children[0].style.transform =
+		'rotate(45deg) translateX(-7.5px) translateY(7px)';
+	burger.children[0].style.width = '20px';
+	burger.children[1].style.transform = '';
+	burger.children[2].style.transformOrigin = 'left';
+	burger.children[2].style.transform =
+		'rotate(-45deg) translateX(-7.5px) translateY(-7px)';
+	burger.children[2].style.width = '20px';
+	const subMenu = document.querySelector(`.mobile-subMenu--${index}`);
+	subMenu.style.right = '0%';
+	subMenu.style.opacity = '1';
+}
+function closeMobileSubMenu() {
+	const burger = document.querySelector('.burger-menu');
+	burger.onclick = closeMenu;
+	burger.children[0].style.transformOrigin = 'center';
+	burger.children[0].style.transform =
+		'translateY(0) translateX(0) rotate(45deg)';
+	burger.children[0].style.width = '40px';
+	burger.children[1].style.transform = 'rotate(45deg) rotate(0deg)';
+	burger.children[2].style.transformOrigin = 'center';
+	burger.children[2].style.transform =
+		'translateY(0) translateX(0) rotate(-45deg)';
+	burger.children[2].style.width = '40px';
+	const subMenus = document.querySelectorAll('.mobile-subMenu');
+	for (const subMenu of subMenus) {
+		subMenu.style.right = '-100%';
+		subMenu.style.opacity = '0';
+	}
 }
 
 // Swiper configuration
