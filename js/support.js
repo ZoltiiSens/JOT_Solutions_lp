ac08111ae56da13095c5be64760028fd00a7aed5
Roman <zoltiisen@gmail.com>
2023-12-18 15:36:04 +0200

    Slider at support page added

--- js/support.js
+++ js/support.js
@@ -163,6 +163,19 @@ function openAnswer(index){
 	questionH2.onclick = openAnswer(index)
 }
 
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    // spaceBetween: 150,
+    initialSlide: 1,
+    centeredSlides: true,
+    loop: false,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
 
 
 
